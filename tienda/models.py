872976ba2ec8fb1e0bd0b699720868cb0f51d50f
hijo del uxiono <dieg.donosop@duocuc.cl>
2024-06-26 16:24:52 -0400

    creacion 2 tablas
    tabla para productos y tabla para almacenar mensajes de contacto
    ademas de crear la carpeta media para almacenar las imagenes que se agreguen desde admin

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Productos(models.Model):
+    id_produ        = models.AutoField(db_column = 'idProdu', primary_key=True)
+    nombre_producto = models.CharField(max_length=50)
+    precio          = models.IntegerField()
+    foto            = models.ImageField(upload_to='media', null=True)
+
+
+class Contacto(models.Model):
+    id_contacto     = models.AutoField(db_column = 'idContacto', primary_key=True)
+    nombrepri       = models.CharField(max_length=50)
+    apellidopa      = models.CharField(max_length=50)
+    telefono        = models.IntegerField()
+    email           = models.EmailField()
+    mensaje         = models.TextField()
